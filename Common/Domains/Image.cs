9e33ef565b102bb5561b23b5adf03bdf7cbed438
emre kurt <emrekurt027@outlook.com>
2019-03-20 23:31:05 +0300

    Revert "images class"
    This reverts commit 245a857e1339d02ae25e5f0abb246a9e52953769.

--- Common/Domains/Image.cs
+++ Common/Domains/Image.cs
@@ -11,6 +11,7 @@ namespace Common.Domains
         public int Id { get; set; }
         public string URL { get; set; }
         public string fas3 { get; set; }
+        public string fas343 { get; set; }
         public int ProductId { get; set; }
         public int ProductId2 { get; set; }
     }
